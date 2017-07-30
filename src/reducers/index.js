4d6183d8e311f8183836b2adb1271d3ff1a31fb4
Jadir J Silva Junior <jadirjsjunior@gmail.com>
2017-07-30 12:43:27 -0300

    redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as FormReducer } from 'redux-form';
 import ReducerPosts from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: ReducerPosts
+  posts: ReducerPosts,
+  form: FormReducer
 });
 
 export default rootReducer;
